db05c7af75099f33c81ab1a7d283c196a4713d7c
YOUNG <12171147+eliasyoung@users.noreply.github.com>
2024-11-25 14:46:33 +0800

    adjust xai features implementation

--- api/core/tools/provider/builtin/xai_power_set/tools/xai_like_tweets.py
+++ api/core/tools/provider/builtin/xai_power_set/tools/xai_like_tweets.py
@@ -1,4 +1,3 @@
-import logging
 from typing import Any, Union
 
 from httpx import post
@@ -8,9 +7,23 @@ from core.tools.entities.tool_entities import ToolInvokeMessage
 from core.tools.tool.builtin_tool import BuiltinTool
 
 XAI_LIKE_TWEETS_PATH = "adminApi/supportTwitter"
-
+TEST_PING_PATH = "test/ping"
 
 class XAILikeTweets(BuiltinTool):
+
+    def validate_credentials(self, credentials: dict[str, Any], tool_parameters: dict[str, Any]) -> None:
+        api_url = credentials.get('xai_api_url')
+
+        if not api_url or api_url == "":
+            raise Exception("xai api url is required!")
+        
+        url = URL(api_url) / "api" / TEST_PING_PATH if 'api' not in api_url else URL(api_url) / TEST_PING_PATH
+
+        response = post(str(url))
+
+        if response.status_code != 200:
+            raise Exception("connect to xai api failed")
+
     def _invoke(self,
                 user_id: str,
                 tool_parameters: dict[str, Any],
@@ -19,7 +32,11 @@ class XAILikeTweets(BuiltinTool):
         api_url = self.runtime.credentials.get('xai_api_url', None)
         accountUsername = tool_parameters.get("tweets_user_name", "")
         tweetsUrl = tool_parameters.get("twitter_url", "")
-        sync = tool_parameters.get("sync", None)
+        
+        if tool_parameters.get("sync", False):
+            sync = "true"
+        else:
+            sync = "false"
 
         if accountUsername is None or accountUsername == "":
             raise Exception("账号不能为空！")
@@ -30,17 +47,7 @@ class XAILikeTweets(BuiltinTool):
         if sync is None:
             raise Exception("阻塞传递未设置或参数错误！")
 
-
         url = URL(api_url) / "api" / XAI_LIKE_TWEETS_PATH if 'api' not in api_url else URL(api_url) / XAI_LIKE_TWEETS_PATH
-
-        # data = {
-        #     "url": str(url),
-        #     "tweets": tweetsUrl,
-        #     "username": accountUsername,
-        #     "sync": sync
-        # }
-
-        # return self.create_json_message(data)
 
         data = {
             "tweets_user_name": accountUsername,
@@ -51,12 +58,12 @@ class XAILikeTweets(BuiltinTool):
         try:
             response = post(str(url), json=data)
             if response.status_code == 400:
-                raise Exception(f"Failed to like tweets: {tweetsUrl} with user: {accountUsername}")
+                raise Exception(f'Failed to distribute task: like tweets: {tweetsUrl} with user: {accountUsername}')
         except:
-            raise Exception(f"Failed to like tweets: {tweetsUrl} with user: {accountUsername}")
+            raise Exception(f'Failed to distribute task: like tweets: {tweetsUrl} with user: {accountUsername}')
         
         if response.status_code == 200 and isinstance(response.text, str):
-            return self.create_text_message(f"User: {accountUsername} like tweets: {tweetsUrl} successfully!")
+            return self.create_text_message(f'Task: User {accountUsername} like tweets {tweetsUrl} distribute successfully!')
         else:
-            raise Exception(f"Failed to like tweets: {tweetsUrl} with user: {accountUsername}")
+            raise Exception(f'Failed to distribute task: like tweets: {tweetsUrl} with user: {accountUsername}')
        
--- api/core/tools/provider/builtin/xai_power_set/xai_power_set.py
+++ api/core/tools/provider/builtin/xai_power_set/xai_power_set.py
@@ -1,40 +1,24 @@
 from typing import Any
 
-from httpx import post
-from yarl import URL
-
 from core.tools.errors import ToolProviderCredentialValidationError
 from core.tools.provider.builtin_tool_provider import BuiltinToolProviderController
+from core.tools.provider.builtin.xai_power_set.tools.xai_like_tweets import XAILikeTweets
 
-TEST_PING_PATH = "test/ping"
-
-class TGAIPowerSetProvider(BuiltinToolProviderController):
+class XAIPowerSetProvider(BuiltinToolProviderController):
     def _validate_credentials(self, credentials: dict[str, Any]) -> None:
         try:
-            # TGAIKvPost().fork_tool_runtime(
-            #     runtime={
-            #         "credentials": credentials,
-            #     }
-            # ).invoke(
-            #     user_id='',
-            #     tool_parameters={
-            #         "kv_key": "tgai_kvdb_test_key",
-            #         "kv_value": "tgai_kvdb_test_value"
-            #     },
-            # )
-
-            api_url = credentials.get('xai_api_url', None)
-
-            if api_url is None or api_url == "":
-                raise Exception
-            
-            url = URL(api_url) / "api" / TEST_PING_PATH if 'api' not in api_url else URL(api_url) / TEST_PING_PATH
-
-            response = post(str(url))
-
+            XAILikeTweets().fork_tool_runtime(
+                runtime={
+                    "credentials": credentials,
+                }
+            ).validate_credentials(
+                credentials=credentials,
+                tool_parameters={}
+            )
 
         except Exception as e:
             # raise ToolProviderCredentialValidationError(str(e))
+            logging.info(e)
             raise ToolProviderCredentialValidationError("无法连接到 XAI 服务，请检查 API 地址是否正确！")
             
     
