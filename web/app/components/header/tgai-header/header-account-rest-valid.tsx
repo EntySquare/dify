4fb344b419cfbe4acf97678932403fd4a47b5afb
guodayang <763917746@qq.com>
2024-12-02 20:53:34 +0800

    添加容器编译 xai变量

--- web/app/components/header/tgai-header/header-account-rest-valid.tsx
+++ web/app/components/header/tgai-header/header-account-rest-valid.tsx
@@ -1,27 +1,27 @@
-"use client";
+'use client'
 
-import { TGAIGet } from "@/service/http";
-import classNames from "@/utils/classnames";
-import dayjs from "dayjs";
-import useSWR from "swr";
+import dayjs from 'dayjs'
+import useSWR from 'swr'
+import { TGAIGet } from '@/service/http'
+import classNames from '@/utils/classnames'
 
 export const HeaderAccountRestValid = () => {
-  const { data } = useSWR(["/manage/checkExpiration"], () =>
-    TGAIGet<number>("/manage/checkExpiration"),
-  );
+  const { data } = useSWR(['/manage/checkExpiration'], () =>
+    TGAIGet<number>('/manage/checkExpiration'),
+  )
 
   return (
     <>
       {data && (
         <div
           className={classNames(
-            "font-bold text-tgai-text-1"
+            'font-bold text-tgai-text-1',
           )}
         >
           <span className="text-tgai-primary">账户有效期至:</span>
-          {dayjs(data.data * 1000).format("YYYY年MM月DD日 hh:mm:ss")}
+          {dayjs(data.data * 1000).format('YYYY年MM月DD日 hh:mm:ss')}
         </div>
       )}
     </>
-  );
-};
+  )
+}
--- web/app/components/header/tgai-header/index.tsx
+++ web/app/components/header/tgai-header/index.tsx
@@ -1,35 +1,32 @@
-"use client";
+'use client'
 
-import Image from "next/image";
-import { HeaderUserProfile } from "./header-user-profile";
-import { HeaderAccountRestValid } from "./header-account-rest-valid";
-import { HeaderThemeSwitcher } from "./header-theme-switcher";
-import classNames from "@/utils/classnames";
-import { TGAILogo } from "@/app/components/base/logo/tgai-logo";
+import { HeaderUserProfile } from './header-user-profile'
+import { HeaderThemeSwitcher } from './header-theme-switcher'
+import classNames from '@/utils/classnames'
+import { TGAILogo } from '@/app/components/base/logo/tgai-logo'
 
 type TGAIHeaderProps = {
-  className?: string;
-};
+  className?: string
+}
 
 export function TGAIHeader({ className }: TGAIHeaderProps) {
-
   return (
     <header
       className={classNames(
-        "w-full flex items-center justify-between h-[60px] border-b shadow-xs px-8 bg-tgai-panel-background border-b-tgai-panel-border dark:border-b-stone-600",
+        'w-full flex items-center justify-between h-[60px] border-b shadow-xs px-8 bg-tgai-panel-background border-b-tgai-panel-border dark:border-b-stone-600',
         className,
       )}
     >
       <TGAILogo
         className={classNames(
-          " h-full w-auto py-1 text-tgai-text-1",
+          ' h-full w-auto py-1 text-tgai-text-1',
         )}
       />
       <div className="flex flex-row gap-8 items-center">
-        <HeaderAccountRestValid />
+        {/* <HeaderAccountRestValid /> */}
         <HeaderThemeSwitcher />
         <HeaderUserProfile />
       </div>
     </header>
-  );
+  )
 }
