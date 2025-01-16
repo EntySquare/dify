c3a361991d51a166fad3d680de2cef9a58704071
YOUNG <12171147+eliasyoung@users.noreply.github.com>
2025-01-16 21:06:30 +0800

    feat(enty-chat): add device information to personality selection

--- web/app/components/enty-chat/chat-side-panel/personality-selection.tsx
+++ web/app/components/enty-chat/chat-side-panel/personality-selection.tsx
@@ -3,18 +3,45 @@
 import React, { useCallback, useMemo } from 'react'
 import { useShallow } from 'zustand/react/shallow'
 import { RiErrorWarningLine } from '@remixicon/react'
+import useSWR from 'swr'
 import Input from '@/app/components/base/input'
 import ListItem from '@/app/components/enty-chat/chat-side-panel/list-item'
 import { useEntyAIChatStore } from '@/app/components/enty-chat/store'
-import type { KnowLedge } from '@/service/xai'
+import { getXAIDeviceList } from '@/service/xai'
+import { DeviceInfoType, type KnowLedge } from '@/service/xai'
 import cn from '@/utils/classnames'
 
 type PersonalitySelectionProps = {
   data?: KnowLedge[]
 }
 
+const deviceInfoEnumNameMap: Record<DeviceInfoType, string> = {
+  [DeviceInfoType.X]: 'X',
+  [DeviceInfoType.INSTAGRAM]: 'Instagram',
+  [DeviceInfoType.TRUTHSOCIAL]: 'TruthSocial',
+  [DeviceInfoType.NOT_SET]: '',
+}
+
 const PersonalitySelection = React.memo<PersonalitySelectionProps>(({ data }) => {
   const [search, setSearch] = React.useState('')
+
+  const { data: deviceListData } = useSWR(
+    ['/adminApi/deviceList'],
+    getXAIDeviceList,
+  )
+
+  const personalityAccountPlatformMap = useMemo(() => {
+    if (!deviceListData)
+      return {} as Record<string, DeviceInfoType>
+
+    const res = deviceListData.data.device_list.reduce((pv, device) => {
+      if (device.tweet_account_list.length > 0)
+        pv[device.tweet_account_list[0].tweet_account] = device.info_type
+      return pv
+    }, {} as Record<string, DeviceInfoType>)
+
+    return res
+  }, [deviceListData])
 
   const {
     selectedPersonality,
@@ -67,6 +94,7 @@ const PersonalitySelection = React.memo<PersonalitySelectionProps>(({ data }) =>
                 ? <span className={cn('text-xs truncate w-full')}>{personality.tweet_account}</span>
                 : <span className={cn('text-xs flex flex-row flex-nowrap items-center truncate w-full')}><RiErrorWarningLine className='size-4 stroke-1' />未绑定账号</span>
               }
+              {personality.tweet_account && personalityAccountPlatformMap[personality.tweet_account] !== undefined && <span className='truncate w-full font-semibold text-xs'>{deviceInfoEnumNameMap[personalityAccountPlatformMap[personality.tweet_account]]}</span>}
             </>
           </ListItem>)
           : <div className={'text-tgai-text-2'}>未找到匹配的人设</div>}
